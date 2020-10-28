d820bb40bbcdd0fe60b5f7483158cb9ac5dbbc46
Jérôme Gamez <jerome@gamez.name>
2020-10-28 11:50:57 +0100

    Update application to symfony/* ^5.1.8

--- src/Command/UploadFileCommand.php
+++ src/Command/UploadFileCommand.php
@@ -5,7 +5,6 @@ namespace App\Command;
 use DateTimeImmutable;
 use Kreait\Firebase\Storage;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
